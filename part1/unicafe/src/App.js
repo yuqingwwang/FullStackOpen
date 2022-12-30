705aa2be195a70d7777706a96bdbb3330a839c40
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-30 09:43:14 +0000

    unicafe done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,51 +2,62 @@ import { useState } from 'react'
 
 const scoreDict = {'good': 1, 'neutral': 0, 'bad': -1}
 
-const Header = ({good, neutral, bad, setGood, setNeutral, setBad}) => {
-  console.log({good, neutral, bad})
+const Title = ({ children }) => <h1>{children}</h1>;
 
-  return (<header>
-    <h2>give feedback</h2>
-    <button onClick={() => setGood(good+1)}>good </button>
-    <button onClick={() => setNeutral(neutral+1)}>neutral </button>
-    <button onClick={() => setBad(bad+1)}>bad </button>
-    </header>)
+const Button = ({ text, addFeedback }) => {
+  return (<>
+   <button onClick={addFeedback}>{text}</button>
+    </>)
 }
 
-const Stats = ({good, neutral, bad}) => {
+function StatisticLine({ text, value }) {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+}
+
+const Stats = ({ title, data: { good, neutral, bad } }) => {
   if ((good+neutral+bad) === 0) {
     return (
       <>
-        <h2>statistics</h2>
+        <h2>StatisticLines</h2>
         <p>No feedback given</p>
       </>
     );
   }
+
   const avr = (good*scoreDict['good'] + neutral*scoreDict['neutral'] + bad*scoreDict['bad'])/3
   const positivePercent = good/(good+neutral+bad)
-  return(<><h2>statistics</h2>
-  <table>
-    <tbody>
-      <tr><td>good {good}</td></tr>
-      <tr><td>neutral {neutral}</td></tr>
-      <tr><td>bad {bad}</td></tr>
-      <tr><td>average {avr}</td></tr>
-      <tr><td>positive {positivePercent}</td></tr>
-    </tbody>
-  </table></>)
+
+  return(<>
+   <Title>{title}</Title>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="average" value={avr} />
+          <StatisticLine text="positive" value={positivePercent} />
+        </tbody>
+      </table>
+  </>)
 }
 const App = () => {
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <Header title good={good} neutral={neutral} bad={bad}
-      setGood={setGood} setNeutral={setNeutral} setBad={setBad}/>
-      <Stats good={good} neutral={neutral} bad={bad}/>
-    </div>
+    <>
+      <Title>give feedback</Title>
+      <Button text="good" addFeedback={() => setGood(good + 1)} />
+      <Button text="neutral" addFeedback={() => setNeutral(neutral + 1)} />
+      <Button text="bad" addFeedback={() => setBad(bad + 1)} />
+      <Stats title="StatisticLines" data={{ good, neutral, bad }} />
+    </>
   )
 }
 
