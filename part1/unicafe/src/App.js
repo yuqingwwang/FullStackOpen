7615e5ea8996a743a97e14e2b21bc5b097c21e68
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-30 08:05:38 +0000

    dummy code added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
+
+const Header = (header) => {
+  return (<header>
+    <h2>give feedback</h2>
+    <button>good</button>
+    <button>neutral</button>
+    <button>bad</button>
+  </header>)
+}
+
+const Stats = (stats) => {
+  return(<body><h2>statistics</h2>
+  <p>good 6</p>
+  <p>neutral 2</p>
+  <p>bad 1</p>
+  </body>)
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +25,8 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header title/>
+      <Stats stats/>
     </div>
   )
 }
