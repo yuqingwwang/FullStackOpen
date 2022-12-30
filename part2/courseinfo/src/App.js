90956b8314f9ed94d7ea702701cdd123bda82153
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-30 17:22:32 +0000

    working with  an arbitrary number of courses

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,10 +1,10 @@
-const Header = ({name}) => {
-  return(<>
-  <h1>{name}</h1>
+const Header = ({ name }) => {
+  return (<>
+    <h1>
+      {name}
+    </h1>
   </>)
 }
-
-const Total = ({ exercises }) => <p>Number of exercises {exercises}</p>;
 
 const Part = ({ part, exercises }) => (
   <p>
@@ -16,46 +16,82 @@ const Part = ({ part, exercises }) => (
 // the number of parts a course has
 const Content = ({ parts }) =>
   parts.map((part, i) => (
-    <Part key={i} part={part.name} exercises={part.exercises} />
+    <Part key={i}
+          part={part.name}
+          exercises={part.exercises} />
   ));
 
+const Total = ({ exercises }) => (
+  <p>
+    Number of exercises {exercises}
+  </p>
+);
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  // calculate the sum of exercises with the array method reduce
-  // const sumWithInitial = array1.reduce(
-  //   (accumulator, currentValue) => accumulator + currentValue,
-  //   initialValue
-  // );
+  const Courses = ({ courses }) =>
+    courses.map(function (course, i) {
+      // using array method reduce
 
-  const total = course.parts.reduce((total, part) => total + part.exercises, 0);
+      /*const sumWithInitial =
+      array1.reduce((accumulator, currentValue) =>
+      accumulator + currentValue, initialValue);*/
+
+    const total = course.parts.reduce((total, part)=>
+    total + part.exercises, 0);
+    
+    return(
+      <div key={i}>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total exercises={total} />
+    </div>
+    )
+    }
+  );
 
   return (
     <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={total} />
+    {Courses({courses})}
     </>
   );
 }
