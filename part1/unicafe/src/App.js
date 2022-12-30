fb8c9a94348c57695643ab902a31a7f59b53cddc
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-30 09:16:45 +0000

    button functional

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,34 +1,38 @@
 import { useState } from 'react'
 
 
-const Header = (header) => {
+const Header = ({good, neutral, bad, setGood, setNeutral, setBad}) => {
+
+  console.log({good, neutral, bad})
+
   return (<header>
     <h2>give feedback</h2>
-    <button>good</button>
-    <button>neutral</button>
-    <button>bad</button>
-  </header>)
+    <button onClick={() => setGood(good+1)}>good </button>
+    <button onClick={() => setNeutral(neutral+1)}>neutral </button>
+    <button onClick={() => setBad(bad+1)}>bad </button>
+    </header>)
 }
 
 const Stats = ({good, neutral, bad}) => {
   return(<><h2>statistics</h2>
   <table>
     <tbody>
-      <tr>good {good}</tr>
-      <tr>neutral {neutral}</tr>
-      <tr>bad {bad}</tr>
+      <tr><td>good {good}</td></tr>
+      <tr><td>neutral {neutral}</td></tr>
+      <tr><td>bad {bad}</td></tr>
     </tbody>
   </table></>)
 }
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header title/>
+      <Header title good={good} neutral={neutral} bad={bad}
+      setGood={setGood} setNeutral={setNeutral} setBad={setBad}/>
       <Stats good={good} neutral={neutral} bad={bad}/>
     </div>
   )
