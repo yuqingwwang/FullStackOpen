435c3fc1f79aff4a639c903d5ded7f6e2c8d0473
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-02 00:26:12 +0000

    starting with phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,16 +6,26 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
+      <form onSubmit={addName}>
+        <input value={newName} onChange={handleNameChange} />
           <button type="submit">add</button>
-        </div>
       </form>
       <h2>Numbers</h2>
       ...
