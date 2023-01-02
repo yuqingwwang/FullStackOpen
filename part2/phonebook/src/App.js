fba2983912e3380c8f2a2539c5e7d4283d711936
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-02 23:45:45 +0000

    step 4 working

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,40 @@
 import { useState } from 'react'
 
-const Persons = ({ name }) => {
+const Person = ({ person}) => (
+  <li>
+    {person.name} {person.number}
+    {/* <button onClick={() => deletePerson(person.id)}>Delete</button> */}
+  </li>
+)
+
+const Persons = ({ persons, deletePerson }) => {
   return (
-    <li key='name.id'>{name.name} {name.number}</li>
+    <div>
+      <ul>
+        {persons
+          .map(person =>
+            <Person
+              key={person.id}
+              person={person}
+            />
+        )}
+      </ul>
+    </div>
   )
 }
+
+const Filter = ({ filter, handleFilter }) => (
+  <div>
+    <form>
+      <div>
+        filter shown with <input
+          value={filter}
+          onChange={handleFilter}
+        />
+      </div>
+    </form>
+  </div>
+)
 
 const App = ( props ) => {
   const [persons, setPersons] = useState([
@@ -13,8 +43,13 @@ const App = ( props ) => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newPerson, setNewPerson] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter ] = useState('')
+
+  const personsAfterFilter = filter === ''  ? persons : persons.filter(person =>
+      person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -44,9 +79,19 @@ const App = ( props ) => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
   return(
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <Filter
+        filter={filter}
+        handleFilter={handleFilter}
+      />
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>Name: <input
             value={newPerson}
@@ -59,11 +104,14 @@ const App = ( props ) => {
         <button type="submit">save</button>
       </form>
       <h2>Numbers</h2>
-      <ul>
+      <Persons
+        persons={personsAfterFilter}/>
+
+      {/* <ul>
         {persons.map(person =>
           <Persons key={person.id} name={person}/>
         )}
-      </ul>
+      </ul> */}
     </div>
   )
   }
