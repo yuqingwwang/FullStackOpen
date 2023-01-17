a38bef779f9538be036efc03720e71f4a22d87a1
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-17 03:21:18 +0000

    -mchanging some codes in the instruction to pass the test, not sure if I did something wrong or the instruction is outdated

--- notes_app/notes_backend/app.js
+++ notes_app/notes_backend/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 require('express-async-errors')
 
+const loginRouter = require('./controllers/login')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
@@ -28,6 +29,8 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
