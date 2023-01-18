dad1d850553f7124f3bc93174318c523f8d6e409
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-18 23:37:26 +0000

    added restriction to username and pwd length

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -2,8 +2,22 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { title: 1, author: 1, url: 1 })
+
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (password.length<4){
+    return response.status(400).json({
+      error: 'password must be at least three characters'
+    })
+  }
 
   const existingUser = await User.findOne({ username })
   if (existingUser) {
@@ -24,11 +38,6 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
-})
-
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
 })
 
 module.exports = usersRouter
--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true
+  },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -22,6 +29,4 @@ userSchema.set('toJSON', {
   }
 })
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)
