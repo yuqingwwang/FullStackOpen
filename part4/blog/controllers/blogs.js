ad9d71ff00fe50bd2f2d5d8c8edee42a62ce9875
Yuqing Wang <yuqingwang42@gmail.com>
2023-01-18 23:57:20 +0000

    added populate

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
+blogsRouter.get('/', async(request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
 })
 
 blogsRouter.get('/:id', async(request, response) => {
@@ -20,9 +19,21 @@ blogsRouter.get('/:id', async(request, response) => {
 })
 
 blogsRouter.post('/', async(request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
   const savedBlogs = await blog.save()
+  user.blogs = user.blogs.concat(savedBlogs._id)
+  await user.save()
   response.status(201).json(savedBlogs)
 })
 
