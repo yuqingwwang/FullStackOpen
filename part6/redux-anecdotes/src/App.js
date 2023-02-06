10e66449c6aade5c28411ce70109f26792d80298
Yuqing Wang <yuqingwang42@gmail.com>
2023-02-06 19:38:12 +0000

    notification implemented

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,11 +2,13 @@
 import AnecdoteForm from "./components/AnecdoteForm"
 import AnecdoteList from "./components/AnecdoteList"
 import Filter from "./components/Filter"
+import Notification from './components/Notification'
 
 const App = () => {
 
   return (
     <div>
+      <Notification />
       <Filter/ >
       <h2>create new</h2>
       <AnecdoteForm/ >
--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,12 +1,27 @@
+import { useSelector } from 'react-redux'
+
 const Notification = () => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
+  const notification = useSelector(state => state.notification)
+
+  if (typeof notification === 'undefined'){
+    return <div style={style}>
+    hello and welcome
+  </div>
+  }
+
+  if (notification === null) {
+    return null
+  }
+
   return (
     <div style={style}>
-      render here notification...
+      {notification}
     </div>
   )
 }
