29eeee977682a95ea58aff503fd274417bba77dc
Yuqing Wang <yuqingwang42@gmail.com>
2023-02-07 23:06:07 +0000

    initial commit; load data from server

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,62 @@
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  const result = useQuery(
+    'anecdotes',
+    getAnecdotes,
+    {
+      retry: false
+    }
+  )
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
