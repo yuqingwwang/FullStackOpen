eba8ea013cca1a542c449beee8396d92126fc434
Yuqing Wang <yuqingwang42@gmail.com>
2023-02-13 00:22:48 +0000

    initial commit

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useCountry } from "./hooks/useCountry"
+import { useField} from "./hooks/useField"
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/country-hook/src/hooks/useCountry.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => {
+          console.log(response)
+          setCountry( { ...response.data[0], found: true })
+        })
+        .catch((error) => {
+          setCountry({ found: false })
+        })
+    }
+  }, [name])
+
+  return country
+}
