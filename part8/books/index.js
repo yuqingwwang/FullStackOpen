04a94187d91d3ab3217dd69ec7cba297ecdaf086
Yuqing Wang <bibolikepeaches@gmail.com>
2023-03-31 12:28:58 +0100

    query allBooks

--- part8/books/index.js
+++ part8/books/index.js
@@ -104,28 +104,26 @@ const typeDefs = `
     born: Int
   }
 
-  type Genre {
-    name: String!
-  }
-  
   type Book {
     title: String!
     published: Int!
     author: String!
     id: ID!
-    genres: [Genre!]!
+    genres: [String!]
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
