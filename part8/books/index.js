711be78f0baced06b9f82ff65255f6de51f22b9f
Yuqing Wang <bibolikepeaches@gmail.com>
2023-03-31 13:23:27 +0100

    Books by genre

--- part8/books/index.js
+++ part8/books/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,8 +126,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author) {
+      if (args.author && args.genre){
+        return books.filter(book => book.author === args.author && book.genres.includes(args.genre));
+      }
+      else if (args.author) {
         return books.filter(book => book.author === args.author);
+      }
+      else if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre));
       }
       return books;
     },
