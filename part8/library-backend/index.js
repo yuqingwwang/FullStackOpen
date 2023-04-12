5a38c6e0bb9ef701c0a1720dac4698ab13a83413
Yuqing Wang <bibolikepeaches@gmail.com>
2023-04-12 18:32:03 +0100

    Subscriptions on the server

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,11 +1,14 @@
 const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+// const { startStandaloneServer } = require('@apollo/server/standalone')
 const { expressMiddleware } = require('@apollo/server/express4')
 const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 const express = require('express')
 const cors = require('cors')
 const http = require('http')
+
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
 
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
@@ -35,9 +38,28 @@ mongoose
     const app = express()
     const httpServer = http.createServer(app)
 
+    const wsServer = new WebSocketServer({
+      server: httpServer,
+      path: '/',
+    })
+
+    const schema = makeExecutableSchema({ typeDefs, resolvers })
+    const serverCleanup = useServer({ schema }, wsServer)
+
     const server = new ApolloServer({
-      schema: makeExecutableSchema({ typeDefs, resolvers }),
-      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+      schema,
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+        {
+          async serverWillStart() {
+            return {
+              async drainServer() {
+                await serverCleanup.dispose();
+              },
+            };
+          },
+        },
+      ],
     })
 
     await server.start()
