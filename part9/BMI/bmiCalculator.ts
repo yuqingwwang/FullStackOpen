e23c278852e758f63f9cbd5a0f34b9676e35466e
Yuqing Wang <bibolikepeaches@gmail.com>
2023-04-19 11:33:18 +0100

    bmiCalculator working with command line

--- part9/BMI/bmiCalculator.ts
+++ part9/BMI/bmiCalculator.ts
@@ -1,17 +1,20 @@
 const calculateBmi = (height: number, weight: number) => {
   const BMI = weight / ((height / 100) * (height / 100))
   if ( BMI > 18.5 ||BMI< 24.9){
-    return 'Normal (healthy weight)'
+    console.log('Normal (healthy weight)')
   }
   else if (BMI <= 18.5){
-    return 'Underweight'
+    console.log('Underweight')
   }
   else if (BMI > 25 || BMI < 29.9){
-    return 'Overweight'
+    console.log('Overweight')
   }
   else{
-    return 'Obese'
+    console.log('Obese')
   }
 }
 
-console.log(calculateBmi(180, 74))
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+
+calculateBmi(height, weight)
