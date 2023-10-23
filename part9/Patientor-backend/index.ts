ef7f972affcbb0b8b04e8716737b9bde8ce0275c
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-23 04:32:49 +0100

    integrated

--- part9/Patientor-backend/index.ts
+++ part9/Patientor-backend/index.ts
@@ -1,13 +1,30 @@
 import express from 'express';
 import diagnosesRouter from './src/routes/diagnoses';
 import diagnosisService from './src/services/diagnosisService';
+import patientService from './src/services/patientService';
+import patientsRouter from './src/routes/patients';
+
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+
+const corsOptions ={
+  origin:'http://localhost:5173',
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+};
+
+app.use(cors(corsOptions));
 
 const PORT = 3000;
 
 app.get('/api/diagnoses', (_req, res) => {
   res.send(diagnosisService.getEntries());
+});
+
+app.get('/api/patients', (_req, res) => {
+  res.send(patientService.getNonSensitiveEntries());
 });
 
 app.get('/api/ping', (_req, res) => {
@@ -16,6 +33,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
